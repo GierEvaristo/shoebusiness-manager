c76bddc5703d5ad647656af124e15266bcc8737e
ongbryce418 <bryceo_ong@dlsu.edu.ph>
2022-06-15 15:38:55 +0800

    array

--- lib/services/orders.dart
+++ lib/services/orders.dart
@@ -6,9 +6,11 @@ class Order {
   late String name;
   late String address;
   late String number;
+  late dynamic date;
+  late bool status;
 
   Order(
-      {required this.name, required this.address, required this.number}) {
+      {required this.name, required this.address, required this.number, required this.date, required this.status}) {
   }
 
   static Order fromJson(Map<String, dynamic> json) {
@@ -16,6 +18,8 @@ class Order {
       name: json['name'],
       address: json['address'],
       number: json['contact number'],
+      status: json['completed'],
+      date: json['date'],
 
     );
   }
